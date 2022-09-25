ff1a534da8983221783b97ecd9a755bc5d735063
Timothy Sipahutar <ifs19016@students.del.ac.id>
2022-09-26 01:23:02 +0700

    filter

--- public/scripts/app.example.js
+++ public/scripts/app.example.js
@@ -14,12 +14,36 @@ class App {
   }
 
   run = () => {
-    Car.list.forEach((car) => {
+    this.clear();
+    const result = this.filterCarByRequest();
+
+    result.forEach((car) => {
       const node = document.createElement("div");
       node.innerHTML = car.render();
       this.carContainerElement.appendChild(node);
     });
   };
+
+  filterCarByRequest() {
+    const requestDate = document.getElementById("date").value;
+    const requestTime = document.getElementById("time").value;
+    const requestCapacity = document.getElementById("capacity").value;
+
+    const timestamp = new Date(`${requestDate} ${requestTime}`);
+    console.log(timestamp);
+
+    if (timestamp == "Invalid Date") {
+      alert("Invalid date input, please choose correctly");
+      return;
+    } else if (requestCapacity <= 0 || requestCapacity > 6) {
+      alert("Invalid, max capacity is only 6!");
+      return;
+    } else {
+      return Car.list.filter(
+        (car) => car.capacity >= requestCapacity && car.availableAt <= timestamp
+      );
+    }
+  }
 
   async load() {
     const cars = await Binar.listCars();
